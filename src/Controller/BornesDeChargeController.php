ef0a608699a644c911c8e365656df4b7480ebe27
SaifSardahi <saifsardahi@gmail.com>
2021-06-24 14:47:13 +0200

    new bdd

--- src/Controller/BornesDeChargeController.php
+++ src/Controller/BornesDeChargeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Operateur;
+use App\Entity\Station;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,5 +19,30 @@ class BornesDeChargeController extends AbstractController
             'controller_name' => 'BornesDeChargeController',
         ]);
     }
-}
 
+    /**
+     * @Route("/bdd", name="base_de_donnee")
+     */
+    public function bdd()
+    {
+        $url = "https://www.data.gouv.fr/fr/datasets/fichier-consolide-des-bornes-de-recharge-pour-vehicules-electriques/";
+
+        $handle = fopen($url, 'r');
+        $countline = true;
+        while ($line = fgetcsv($handle, 0, ',')) {
+            if ($countline) {
+                $countline = false;
+                continue;
+            }
+
+            $d = new Operateur();
+            $d->setnom($line[0]);
+            $d->setnumSiren($line[1]);
+            $d->setmail($line[2]);
+            // $d->adresse($line[0]);
+            // $d->tarif($line[0]);
+            // $d->date_debut_contrat($line[0]);
+            // $d->date_fin_contrat($line[0]);
+        }
+    }
+}
