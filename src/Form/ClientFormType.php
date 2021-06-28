c4f88727752df3e51aa5fc284d6bd15f831eee7b
Cecilia <hiribarne.cecilia@gmail.com>
2021-06-28 17:10:27 +0200

    formulaire client

---
+++ src/Form/ClientFormType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Client;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ClientFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class,['mapped' => false])
+            ->add('password', RepeatedType::class,[
+                'type' => PasswordType::class,
+                'invalid message' => 'le mot de passe est différent',
+                'required' => true,
+                'first_verification' => ['label' => 'entrez votre mot de passe'],
+                'second_verification' => ['entrez de nouveau votre mot de passe'],
+                'mapped' => false],)
+            ->add('nom', TextType::class,['label' => 'nom'])
+            ->add('prenom',TextType::class,['label' => 'prenom'])
+            ->add('mail',EmailType::class)
+            ->add('adresse',TextType::class,['label' => 'adresse'])
+            ->add('tel',TelType::class,['label' => 'tel'])
+            ->add('date_debut_contrat', DateType::class)
+
+            ->add('validate', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Client::class,
+        ]);
+    }
+}
