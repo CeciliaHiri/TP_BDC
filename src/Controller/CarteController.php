916c11c7d1616a86a2dad28069208c27e52e15dd
openkeha <keha.open@gmail.com>
2021-07-01 17:08:59 +0200

    add map to index

--- src/Controller/CarteController.php
+++ src/Controller/CarteController.php
@@ -7,19 +7,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\StationRepository;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class CarteController extends AbstractController
 {
     /**
      * @Route("/map", name="map")
      */
-    public function map(StationRepository $station): Response
+    public function map(StationRepository $station, SerializerInterface $serializer): Response
     {
-        $station = $station->findAll();
+        $stations = $station->findAll();
+        $stationss = array();
+        foreach ($stations as $s) {
+            $stationss[] = $serializer->serialize($s, 'json', ['groups' => ['listStation']]);
+        }
 
         return $this->render(
             'carte/index.html.twig',
-            array('station' => $station)
+            array('station' => $stationss)
         );
     }
 }
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,19 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\StationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class IndexController extends AbstractController
 {
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(StationRepository $station, SerializerInterface $serializer): Response
     {
+        $stations = $station->findAll();
+        $stationss = array();
+        foreach ($stations as $s) {
+            $stationss[] = $serializer->serialize($s, 'json', ['groups' => ['listStation']]);
+        }
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
+            'station' => $stationss,
         ]);
     }
 }
