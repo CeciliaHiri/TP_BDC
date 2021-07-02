e8eb1caf9b3439e53a43a7562a107130b3f216cc
openkeha <keha.open@gmail.com>
2021-07-02 14:39:14 +0200

    correction app_login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,6 +14,14 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        if ($this->getUser()) {
+            if ($this->getUser()->getRoles() === ["ROLE_ADMIN","ROLE_USER"]) {
+                return $this->redirectToRoute('admin');
+            }
+            if ($this->getUser()->getRoles() === ["ROLE_USER"]) {
+                return $this->redirectToRoute('espace_profile');
+            }
+        }
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
